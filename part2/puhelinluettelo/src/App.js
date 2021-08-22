dc8d53c70c682007f0fea3f0705de323e9954ee5
unknown <miettinen1tuomo@gmail.com>
2021-08-23 01:45:47 +0300

    p2b

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import Filter from './components/Filter'
 
-function App() {
+
+
+
+
+
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+  
+  const addPerson = (e) => {
+    e.preventDefault();
+    var alreadyExists = false;
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newName || persons[i].number === newNumber ) {
+        alert(newName + ' is already added to phonebook');
+        alreadyExists = true;
+        break;
+      }
+    }
+    if (!alreadyExists) setPersons(persons.concat({ name: newName , number: newNumber }));
+    setNewNumber('');
+    setNewName('');
+    
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
+  }
+  const handleFilterChange = (e) => {
+    setNewFilter(e.target.value)
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      <h1>add a new</h1>
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      
+      <h2>Numbers</h2>
+      <Persons persons={persons} newFilter={newFilter}/>
+      
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
