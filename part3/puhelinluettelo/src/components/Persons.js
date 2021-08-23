d688bfce801ab4e83e3cea7d071593c8ff4a6471
unknown <miettinen1tuomo@gmail.com>
2021-08-24 02:40:10 +0300

    p3.8

---
+++ part3/puhelinluettelo/src/components/Persons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Persons = (props) => {
+    let pts = props.persons.filter(p => p.name.includes(props.newFilter));
+    return(
+      <div>
+        {pts.map((p, i) => <p key={i}>{p.name} {p.number} <button onClick={() => props.deletePerson(p.id)}>delete</button></p>)}
+      </div>
+    )
+  }
+
+export default Persons
