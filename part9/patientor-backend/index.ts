133cf5ae34e4bbf7b57e5ea8af2d545192c23262
unknown <miettinen1tuomo@gmail.com>
2022-01-25 19:43:03 +0200

    9.10

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import patientRouter from './src/routes/patients'
+import diagnosesRouter from './src/routes/diagnoses'
 
 const cors = require('cors')
 const app = express();
@@ -12,6 +14,10 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/patients', patientRouter);
+app.use('/api/diagnoses', diagnosesRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/types.ts
@@ -0,0 +1,17 @@
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+
+export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment?: string;
+}
+
+export interface DiagnosisEntry {
+  code: string;
+  name: string;
+  latin?: string;
+}
